5c46b25e50cbdec593e41fff9d68ea4a7240543e
vitoralveschaves <vitorchavesalv@gmail.com>
2024-08-14 22:11:43 -0300

    feat: add FoodRepository interface

---
+++ src/main/java/com/example/pagination/repositories/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.example.pagination.repositories;
+
+import com.example.pagination.entities.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
